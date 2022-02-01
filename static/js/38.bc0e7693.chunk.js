4de1357f4a2abf83143413e61436120dd87dfdd0
NRH <n.riccahang674@gmail.com>
2022-02-01 06:26:07 -0400

    update 01/02/2022

---
+++ static/js/38.bc0e7693.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpprojet-uber-eats-dashboard"]=this["webpackJsonpprojet-uber-eats-dashboard"]||[]).push([[38],{796:function(e,s,t){"use strict";t.r(s);t(0);var c=t(174),r=t(2);s.default=function(){return Object(r.jsxs)("div",{children:[Object(r.jsx)(c.e,{}),Object(r.jsxs)("div",{className:"wrapper d-flex flex-column min-vh-100 bg-light",children:[Object(r.jsx)(c.d,{}),Object(r.jsx)("div",{className:"body flex-grow-1 px-3",children:Object(r.jsx)(c.b,{})}),Object(r.jsx)(c.c,{})]})]})}}}]);
+//# sourceMappingURL=38.bc0e7693.chunk.js.map
